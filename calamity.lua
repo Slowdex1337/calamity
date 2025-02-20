47802cc6167693bb80aa536b5cae344f8bc7bb0b
Slowdex1337 <maslovm988@gmail.com>
2025-02-21 01:18:22 +0300

    Update calamity.lua

--- calamity.lua
+++ calamity.lua
@@ -1821,7 +1821,7 @@ client.set_event_callback('paint', function()
     end
 
     if lua_menu.misc.airqsbind:get() and lua_menu.misc.airqs:get() then
-        renderer.indicator(255, 255, 255, 255, 'QS')
+        renderer.indicator(255, 255, 255, 255, 'Jump Scout')
     end
 end)
 
@@ -1872,3 +1872,400 @@ client.set_event_callback("paint_ui", function()
     end
     renderer.indicator(240 , 240 , 240 , 240, "Target: " .. target)
 end)	
+-- local variables for API functions. any changes to the line below will be lost on re-generation
+local client_visible, client_eye_position, client_log, client_trace_bullet, entity_get_bounding_box, entity_get_local_player, entity_get_origin, entity_get_player_name, entity_get_player_resource, entity_get_player_weapon, entity_get_prop, entity_is_dormant, entity_is_enemy, globals_curtime, globals_maxplayers, globals_tickcount, math_max, renderer_indicator, string_format, ui_get, ui_new_checkbox, ui_new_hotkey, ui_reference, ui_set_callback, sqrt, unpack, entity_is_alive, plist_get = client.visible, client.eye_position, client.log, client.trace_bullet, entity.get_bounding_box, entity.get_local_player, entity.get_origin, entity.get_player_name, entity.get_player_resource, entity.get_player_weapon, entity.get_prop, entity.is_dormant, entity.is_enemy, globals.curtime, globals.maxplayers, globals.tickcount, math.max, renderer.indicator, string.format, ui.get, ui.new_checkbox, ui.new_hotkey, ui.reference, ui.set_callback, sqrt, unpack, entity.is_alive, plist.get
+
+local ffi = require "ffi"
+local vector = require "vector"
+local weapons = require "gamesense/csgo_weapons"
+
+local native_GetClientEntity = vtable_bind("client_panorama.dll", "VClientEntityList003", 3, "void*(__thiscall*)(void*,int)")
+local native_IsWeapon = vtable_thunk(165, "bool(__thiscall*)(void*)")
+local native_GetInaccuracy = vtable_thunk(482, "float(__thiscall*)(void*)")
+
+
+local ref = {
+	mindmg = ui_reference("RAGE", "Aimbot", "Minimum damage"),
+	dormantEsp = ui_reference("VISUALS", "Player ESP", "Dormant"),
+}
+
+local menu = {
+	dormant_switch = ui_new_checkbox("RAGE", "Aimbot", "Dormant aimbot"),
+	dormant_key = ui_new_hotkey("RAGE", "Aimbot", "Dormant aimbot", true),
+	dormant_mindmg = ui.new_slider("RAGE", "Aimbot", "Dormant minimum damage", 0, 100, 10, true),
+	dormant_indicator = ui_new_checkbox("RAGE", "Aimbot", "Dormant indicator"),
+}
+
+local player_info_prev = {}
+local roundStarted = 0
+
+local function modify_velocity(e, goalspeed)
+	local minspeed = math.sqrt((e.forwardmove * e.forwardmove) + (e.sidemove * e.sidemove))
+	if goalspeed <= 0 or minspeed <= 0 then
+		return
+	end
+
+	if e.in_duck == 1 then
+		goalspeed = goalspeed * 2.94117647
+	end
+
+	if minspeed <= goalspeed then
+		return
+	end
+
+	local speedfactor = goalspeed / minspeed
+	e.forwardmove = e.forwardmove * speedfactor
+	e.sidemove = e.sidemove * speedfactor
+end
+
+local function on_setup_command(cmd)
+	if not ui_get(menu.dormant_switch) then
+		return
+	end
+
+	local lp = entity_get_local_player()
+
+	local my_weapon = entity_get_player_weapon(lp)
+	if not my_weapon then
+		return
+	end
+
+	local ent = native_GetClientEntity(my_weapon)
+	if ent == nil or not native_IsWeapon(ent) then
+		return
+	end
+
+	local inaccuracy = native_GetInaccuracy(ent)
+	if inaccuracy == nil then
+		return
+	end
+
+	local tickcount = globals_tickcount()
+	local player_resource = entity_get_player_resource()
+	local eyepos = vector(client_eye_position())
+	local simtime = entity_get_prop(lp, "m_flSimulationTime")
+	local weapon = weapons(my_weapon)
+	local scoped = entity_get_prop(lp, "m_bIsScoped") == 1
+	local onground = bit.band(entity_get_prop(lp, 'm_fFlags'), bit.lshift(1, 0))
+	if tickcount < roundStarted then return end -- to prevent shooting at ghost dormant esp @ the beginning of round
+
+	local can_shoot
+	if weapon.is_revolver then -- for some reason can_shoot returns always false with r8 despite all 3 props being true, no idea why
+		can_shoot = simtime > entity_get_prop(my_weapon, "m_flNextPrimaryAttack") -- doing this fixes it ><
+	elseif weapon.is_melee_weapon then
+		can_shoot = false
+	else
+		can_shoot = simtime > math_max(entity_get_prop(lp, "m_flNextAttack"), entity_get_prop(my_weapon, "m_flNextPrimaryAttack"), entity_get_prop(my_weapon, "m_flNextSecondaryAttack"))
+	end
+
+	-- new player info
+	local player_info = {}
+
+	-- loop through all players and continue if they're connected
+	for player=1, globals_maxplayers() do
+		if entity_get_prop(player_resource, "m_bConnected", player) == 1 then
+			if plist_get(player, "Add to whitelist") then goto skip end
+			if entity_is_dormant(player) and entity_is_enemy(player) then
+				local can_hit
+
+				local origin = vector(entity_get_origin(player))
+				local x1, y1, x2, y2, alpha_multiplier = entity_get_bounding_box(player) -- grab alpha of the dormant esp
+				
+				if player_info_prev[player] ~= nil and origin.x ~= 0 and alpha_multiplier > 0 then -- if origin / dormant esp is valid
+					local old_origin, old_alpha, old_hittable = unpack(player_info_prev[player])
+
+					-- update check
+					local dormant_accurate = alpha_multiplier > 0.795 -- for debug purposes lower this to 0.1
+
+					if dormant_accurate then
+						local target = origin + vector(0, 0, 40)
+						local pitch, yaw = eyepos:to(target):angles()
+						local ent, dmg = client_trace_bullet(lp, eyepos.x, eyepos.y, eyepos.z, target.x, target.y, target.z, true)
+
+						can_hit = (dmg > ui_get(menu.dormant_mindmg)) and (not client_visible(target.x, target.y, target.z)) -- added visibility check to mitigate shooting at anomalies?
+						if can_shoot and can_hit and ui_get(menu.dormant_key) then
+							modify_velocity(cmd, (scoped and weapon.max_player_speed_alt or weapon.max_player_speed)*0.33)
+
+							-- autoscope
+							if not scoped and weapon.type == "sniperrifle" and cmd.in_jump == 0 and onground == 1 then
+								cmd.in_attack2 = 1
+							end
+							
+							if inaccuracy < 0.009 and cmd.chokedcommands == 0 then
+								cmd.pitch = pitch
+								cmd.yaw = yaw
+								cmd.in_attack = 1
+
+								-- dont shoot again
+								can_shoot = false
+								--client_log(string_format('Taking a shot at: %s | tickcount: %d | predcited damage: %d | inaccuracy: %.3f | Alpha: %.3f', entity.get_player_name(player), tickcount, dmg, inaccuracy, alpha_multiplier))
+							end
+						end
+					end
+				end
+				player_info[player] = {origin, alpha_multiplier, can_hit}
+			end
+		end
+		::skip::
+	end
+	player_info_prev = player_info
+end
+
+client.register_esp_flag("DORMANT AIMBOT", 255, 255, 255, function(player)
+	if ui.get(menu.dormant_switch) and entity.is_enemy(player) and player_info_prev[player] ~= nil and entity.is_alive(entity_get_local_player()) then
+		local _, _, can_hit = unpack(player_info_prev[player])
+
+		return can_hit
+	end
+end)
+local function painter()
+	if not entity_is_alive(entity_get_local_player()) then return end -- dont draw if dead :lowiqq:
+	if ui_get(menu.dormant_switch) and ui_get(menu.dormant_key) and ui_get(menu.dormant_indicator) then
+		local colors = {240,240,240,240}
+		for k, v in pairs(player_info_prev) do 
+			if k ~= nil then 
+				if v[3] == true then 
+					colors = {240,240,240,240}
+					break
+				end
+			end
+		end
+		renderer_indicator(colors[1],colors[2],colors[3],colors[4], "DORMANT AIMBOT")
+	end
+end
+local function resetter()
+	local freezetime = (cvar.mp_freezetime:get_float()+1) / globals.tickinterval() -- get freezetime plus 1 second and disable dormantbob for that amount of ticks
+	roundStarted = globals_tickcount() + freezetime
+end
+
+ui_set_callback(menu.dormant_switch, function()
+	local czechbox = ui_get(menu.dormant_switch)
+	local update_callback = czechbox and client.set_event_callback or client.unset_event_callback
+
+	if czechbox then ui.set(ref.dormantEsp, czechbox) end -- enable dormant ESP on ui toggle ( dumb proofing )
+	update_callback("setup_command", on_setup_command)
+	update_callback("paint", painter)
+	update_callback("round_prestart", resetter)
+	ui.set_visible(menu.dormant_indicator, ui_get(menu.dormant_switch))
+	ui.set_visible(menu.dormant_mindmg, ui_get(menu.dormant_switch))
+end)
+ui.set(menu.dormant_indicator, true)
+ui.set_visible(menu.dormant_indicator, ui_get(menu.dormant_switch)) -- yes
+ui.set_visible(menu.dormant_mindmg, ui_get(menu.dormant_switch)) -- yes^2
+
+local ffi = require 'ffi'
+
+local tab, container = "Rage", "Other"
+
+local current = {
+    check_access = true,
+}
+
+ffi.cdef [[
+    struct animation_layer_t {
+		bool m_bClientBlend;		 //0x0000
+		float m_flBlendIn;			 //0x0004
+		void* m_pStudioHdr;			 //0x0008
+		int m_nDispatchSequence;     //0x000C
+		int m_nDispatchSequence_2;   //0x0010
+		uint32_t m_nOrder;           //0x0014
+		uint32_t m_nSequence;        //0x0018
+		float m_flPrevCycle;       //0x001C
+		float m_flWeight;          //0x0020
+		float m_flWeightDeltaRate; //0x0024
+		float m_flPlaybackRate;    //0x0028
+		float m_flCycle;           //0x002C
+		void* m_pOwner;              //0x0030
+		char pad_0038[4];            //0x0034
+    };
+    struct c_animstate { 
+        char pad[ 3 ];
+        char m_bForceWeaponUpdate; //0x4
+        char pad1[ 91 ];
+        void* m_pBaseEntity; //0x60
+        void* m_pActiveWeapon; //0x64
+        void* m_pLastActiveWeapon; //0x68
+        float m_flPrevCycle; //0x001C
+        float m_flWeight; //0x0020
+        float m_flWeightDeltaRate; //0x0024
+        float m_flPlaybackRate; //0x0028
+        float m_flLastClientSideAnimationUpdateTime; //0x6C
+        int m_iLastClientSideAnimationUpdateFramecount; //0x70
+        float m_flAnimUpdateDelta; //0x74
+        float m_flEyeYaw; //0x78
+        float m_flPitch; //0x7C
+        float m_flGoalFeetYaw; //0x80
+        float m_flCurrentFeetYaw; //0x84   
+        float m_flCurrentTorsoYaw; //0x88
+        float m_flUnknownVelocityLean; //0x8C
+        float m_flLeanAmount; //0x90
+        char pad2[ 4 ];
+        float m_flFeetCycle; //0x98
+        float m_flFeetYawRate; //0x9C
+        char pad3[ 4 ];
+        float m_fDuckAmount; //0xA4
+        float m_fLandingDuckAdditiveSomething; //0xA8
+        char pad4[ 4 ];
+        float m_vOriginX; //0xB0
+        float m_vOriginY; //0xB4
+        float m_vOriginZ; //0xB8
+        float m_vLastOriginX; //0xBC
+        float m_vLastOriginY; //0xC0
+        float m_vLastOriginZ; //0xC4
+        float m_vVelocityX; //0xC8
+        float m_vVelocityY; //0xCC
+        char pad5[ 4 ];
+        float m_flUnknownFloat1; //0xD4
+        char pad6[ 8 ];
+        float m_flUnknownFloat2; //0xE0
+        float m_flUnknownFloat3; //0xE4
+        float m_flUnknown; //0xE8
+        float m_flSpeed2D; //0xEC
+        float m_flUpVelocity; //0xF0
+        float m_flSpeedNormalized; //0xF4
+        float m_flFeetSpeedForwardsOrSideWays; //0xF8
+        float m_flFeetSpeedUnknownForwardOrSideways; //0xFC
+        float m_flTimeSinceStartedMoving; //0x100
+        float m_flTimeSinceStoppedMoving; //0x104
+        bool m_bOnGround; //0x108
+        bool m_bInHitGroundAnimation; //0x109
+        float m_flTimeSinceInAir; //0x10A
+        float m_flLastOriginZ; //0x10E
+        float m_flHeadHeightOrOffsetFromHittingGroundAnimation; //0x112
+        float m_flStopToFullRunningFraction; //0x116
+        char pad7[ 4 ]; //0x11A
+        float m_flMagicFraction; //0x11E
+        char pad8[ 60 ]; //0x122
+        float m_flWorldForce; //0x15E
+        char pad9[ 462 ]; //0x162
+        float m_flMaxYaw; //0x334
+        float m_flMinYaw; //0x330
+    };
+]]
+
+local classptr = ffi.typeof('void***')
+local rawientitylist = client.create_interface('client.dll', 'VClientEntityList003') or
+                           error('VClientEntityList003 wasnt found', 2)
+
+local ientitylist = ffi.cast(classptr, rawientitylist) or error('rawientitylist is nil', 2)
+local get_client_networkable = ffi.cast('void*(__thiscall*)(void*, int)', ientitylist[0][0]) or
+                                   error('get_client_networkable_t is nil', 2)
+local get_client_entity = ffi.cast('void*(__thiscall*)(void*, int)', ientitylist[0][3]) or
+                              error('get_client_entity is nil', 2)
+
+local rawivmodelinfo = client.create_interface('engine.dll', 'VModelInfoClient004')
+local ivmodelinfo = ffi.cast(classptr, rawivmodelinfo) or error('rawivmodelinfo is nil', 2)
+local get_studio_model = ffi.cast('void*(__thiscall*)(void*, const void*)', ivmodelinfo[0][32])
+
+
+local misc = {
+    enable = ui.new_checkbox(tab, container, "Cala\a9FCA2BFFMity"),
+    type = ui.new_combobox(tab, container, "Type", {"Default", "Jitter", "Alternative", "Custom"}),
+    delta = ui.new_slider(tab, container, 'Delta', 1, 10, 3, true, "°")
+}
+
+local function NormalizeAngle(angle)
+    while angle > 180 do
+        angle = angle - 360
+    end
+
+    while angle < -180 do
+        angle = angle + 360
+    end
+
+    return angle
+end
+
+local function GetAnimationState(player)
+    if not (player) then
+        return
+    end
+    local player_ptr = ffi.cast("void***", get_client_entity(ientitylist, player))
+    local animstate_ptr = ffi.cast("char*", player_ptr) + 0x9960
+    local state = ffi.cast("struct c_animstate**", animstate_ptr)[0]
+    return state
+end
+
+local eye_yaw = 1
+local ent_name = "none"
+local side = -1
+local side2 = -1
+
+
+local function ResolveJitter(player)
+    local animstate = GetAnimationState(player)
+    local lpent = get_client_entity(ientitylist, player)
+
+    local delta = entity.get_prop(player, "m_angEyeAngles[1]") - entity.get_prop(player, "m_flPoseParameter", 11)
+
+    eye_yaw = animstate.m_flEyeYaw
+
+    ent_name = entity.get_player_name(player)
+
+    local yaws
+
+    local yaw1 = (entity.get_prop(player, "m_flPoseParameter", 11) or 1) * 116 - 58
+
+
+    side = globals.tickcount() % 2 == 0 and -1 or 1
+    side2 = (globals.tickcount() % 3) - 1
+
+    if ui.get(misc.type) == "Default" then
+        yaws = delta * yaw1 * animstate.m_flPlaybackRate
+    elseif ui.get(misc.type) == "Jitter" then
+        yaws = side * math.abs(delta * yaw1 * animstate.m_flPlaybackRate)
+    elseif ui.get(misc.type) == "Alternative" then
+        yaws = side2 * math.abs(delta * yaw1 * animstate.m_flPlaybackRate)
+    else
+        yaws = (delta * yaw1 * animstate.m_flPlaybackRate)/ui.get(misc.delta)
+    end
+
+    yaws = NormalizeAngle(yaws)
+
+    plist.set(player, "Force body yaw", true)
+    plist.set(player, "Force body yaw value", yaws) 
+end
+
+local function Resolver(player)
+    if ui.get(misc.enable) then
+        if entity.is_dormant(player) or entity.get_prop(player, "m_bDormant") then
+            return
+        end
+        ResolveJitter(player)
+    else
+        plist.set(player, "Force body yaw", false)
+    end
+end
+
+local function ResolverUpdate()
+    if current.check_access == false then return end
+    local enemies = entity.get_players(true)
+    for i, enemy_ent in ipairs(enemies) do
+        if enemy_ent and entity.is_alive(enemy_ent) then
+            Resolver(enemy_ent)
+        end
+    end
+end
+
+local x_ind, y_ind = client.screen_size()
+local function paint_indicator()
+    if current.check_access == false then return end
+    if not ui.get(misc.enable) then return end
+    if entity.get_local_player() == nil or not entity.is_alive(entity.get_local_player()) then return end
+    renderer.text(20, y_ind/1.9, 255, 255, 255, 255, "", 0, "> cala\a9FCA2BFFmity \aEE4444FF[beta]")
+    renderer.text(20, y_ind/1.9+12, 255, 255, 255, 255, "", 0, "> resolver type: \aEE4444FF"..ui.get(misc.type))
+    renderer.text(20, y_ind/1.9+24, 255, 255, 255, 255, "", 0, "> Enemy: \aEE4444FF"..ent_name)
+    renderer.text(20, y_ind/1.9+36, 255, 255, 255, 255, "", 0, "> Eye: \aEE4444FF"..math.floor(eye_yaw))
+end
+
+
+
+local function visibility()
+    ui.set_visible(misc.enable, current.check_access)
+    ui.set_visible(misc.type, ui.get(misc.enable) and current.check_access)
+    ui.set_visible(misc.delta, ui.get(misc.type) == "Custom" and ui.get(misc.enable) and current.check_access)
+end
+
+client.set_event_callback("paint_ui", visibility)
+client.set_event_callback("paint", paint_indicator)
+client.set_event_callback("setup_command", ResolverUpdate)
